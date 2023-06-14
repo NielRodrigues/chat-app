dafa278ffaa69813272cbb00d09285dc08b5acd9
Carlos Daniel Rodrigues <101821262+NielRodrigues@users.noreply.github.com>
2023-06-14 10:22:48 -0300

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
@@ -10,5 +11,11 @@ module.exports = {
     timestamp: true,
     underscored: true,
     underscoredAll: true,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 };
